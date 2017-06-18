79cc6d0d75ae9e952c44f8c17d38ac00f489b991
s3341458 <s3341458@student.rmit.edu.au>
2017-06-19 09:54:49 +1000

    gevent server by default listen to 4999

--- gevent_server.py
+++ gevent_server.py
@@ -1,6 +1,6 @@
 from gevent.wsgi import WSGIServer
 from flaskr import app
 
-http_server = WSGIServer(('', 5000), app)
+http_server = WSGIServer(('', 4999), app)
 print("gevent server of flask is running ...")
 http_server.serve_forever()
